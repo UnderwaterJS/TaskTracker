99b8744d5f4310f4979c5d89d3a804697ffa0b35
UnderwaterJS <crazylight.boost2018@mail.ru>
2025-11-28 15:50:07 +0300

    Add new functions and place for database

--- src/main.c
+++ src/main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <time.h>
+#include <sqlite3.h>
 
 void add_task();
 void show_day_tasks();
 void change_task();
 void delete_task();
+void my_total();
 
 typedef struct {
 char title[20];
@@ -14,15 +17,62 @@ char date[20];
 char timing[20];
 char status[20];
 } TASK;
-
+ 
 typedef struct {
 } NODE;
 
+int date_to_days(const char *date_str) {
+    int day, month, year;
+    sscanf(date_str, "%d.%d.%d", &day, &month, &year);
+    int days = 0;
+    for(int y = 1970; y < year; y++) {
+        days += (y % 4 == 0 && (y % 100 != 0 || y % 400 == 0) ? 366 : 365);
+    }
+    int days_in_months[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+        days_in_months[1] = 29;
+    for(int m = 0; m < month - 1; m++) {
+        days += days_in_months[m];
+    }
+    days += day;
+    return days;
+}
+
+float get_duration_from_timing(const char *timing) {
+    int start_min, start_hours, end_min, end_hours;
+    if (sscanf(timing, "%d:%d-%d:%d", &start_hours, &start_min, &end_hours, &end_min)) {
+        int start_total = start_hours * 60 + start_min;
+        int end_total = end_hours * 60 + end_min;
+        int diff_minutes = end_total - start_total;
+        return diff_minutes / 60.0f;
+    }
+    return 0.0f;
+}
+
+int sql() {
+    sqlite3 *db;    // указатель на базу данных
+    // открываем подключение к базе данных
+    int result  = sqlite3_open("TaskTrack.db", &db);
+    // если подключение успешно установлено
+    if(result == SQLITE_OK) 
+    {
+        printf("Connection established\n");
+    }
+    else
+    {
+        // выводим сообщение об ошибке
+        fprintf(stderr, "Error: %s\n", sqlite3_errmsg(db));
+    }
+    // закрываем подключение
+    sqlite3_close(db);
+}
+
 int menu() {
+    sql();
     printf("Welcome to taskTracker!\n");
     while(1) {
     int numb;
-    printf("Avialable optins:\n1.Add task.\n2.Show day tasks.\n3.Change task.\n4.Set reminder.\n5.Show results for the week.\n6.Show results for the month.\n7.Delete task.\n8.Exit\n");
+    printf("\nAvialable optins:\n1.Add task.\n2.Show day tasks.\n3.Change task.\n4.Set reminder.\n5.Show results in some time.\n6.Delete task.\n7.Exit\n");
     printf("Choose option:");
     scanf("%d", &numb);
     switch(numb) {
@@ -39,15 +89,12 @@ int menu() {
         printf("Mind?\n");
     break;
         case 5:
-        printf("Week?\n");
+        my_total();
     break;
         case 6:
-        printf("Month?\n");
-    break;
-        case 7:
         delete_task();
     break;
-        case 8:
+        case 7:
         return 0;
     break;
         default:
@@ -74,7 +121,7 @@ void add_task()
     fgets(my.date, sizeof(my.date), stdin);
     my.date[strcspn(my.date, "\n")] = '\0';
 
-    printf("Enter time period (00:00-00:00): ");
+    printf("Enter time period (hh:mm-hh:mm): ");
     fgets(my.timing, sizeof(my.timing), stdin);
     my.timing[strcspn(my.timing, "\n")] = '\0';
 
@@ -252,6 +299,57 @@ void delete_task() {
         printf("Task not found.\n");
     }
 }
+
+void my_total() {
+    TASK my;
+    char user_period_start[20], user_period_end[20];
+    printf("In what period of time would you like to see results?\n");
+    printf("Please, enter start of period (day.month.year): \n");
+    scanf("%19s", user_period_start);
+    printf("Please, enter end of period (day.month.year): \n");
+    scanf("%19s", user_period_end);
+
+    int start_days = date_to_days(user_period_start);
+    int end_days = date_to_days(user_period_end); 
+
+    FILE *ttk = fopen("tasktracker.txt", "rb");
+    if (ttk == NULL) {
+        printf("Fail to open file\n");
+        return;
+    }
+
+    int workout_count = 0, lesson_count = 0;
+    float programm_hours = 0, job_hours = 0;
+
+    char programming[20] = "Programming";
+    char workout[20] = "Workout";
+    char job[20] = "Job";
+    char lessons[20] = "Lessons";
+
+    int found = 0;
+    while (fread(&my, sizeof(my), 1, ttk) == 1) {
+            int task_days = date_to_days(my.date);
+            if(start_days <= task_days && task_days <= end_days) {
+            if(strcmp(my.title, programming) == 0)
+            programm_hours += get_duration_from_timing(my.timing);
+            else if (strcmp(my.title, workout) == 0)
+            workout_count++;
+            else if (strcmp(my.title, job) == 0)
+            job_hours += get_duration_from_timing(my.timing);
+            else if (strcmp(my.title, lessons) == 0)
+            lesson_count++;
+            found = 1;
+        }
+    }
+    printf("Your efficiency for chosen period:\n");
+    printf("Hours of programming: %.2f\nHours of job: %.2f\nAmount of workouts: %d\nAmount of lessons: %d\n", programm_hours, job_hours, workout_count, lesson_count);
+
+    if (!found) {
+        printf("No tasks found for this date period.\n");
+    }
+    fclose(ttk);
+}
+
 
 int main() {
     menu();
